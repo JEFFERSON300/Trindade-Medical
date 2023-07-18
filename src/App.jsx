09c710ea3aefbbaa9212d5d9f39f814f8e5e1a0e
Jefferson Pedro dos Santos Silva <jeffersonpedro300@gmail.com>
2023-07-18 13:15:00 -0300

    Inclusão de modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { LoginPage } from "./pages/Login/Login.page"
-
+import { LoginPage } from "./pages/Login/Login.page";
 
 function App() {
-  
-
   return (
     <div>
-      <LoginPage/>
+      <LoginPage />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Form/Login/Login.component.jsx
+++ src/components/Form/Login/Login.component.jsx
@@ -1,17 +1,19 @@
-export const FormLoginComponent = () => {
-    return (
-    <form>
-        <div className="input-group"> 
-        <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" placeholder="Digite seu email"/>
-        </div>
+import ButtonNewUserComponent from "../ButtonNewUser/ButtonNewUser.component";
 
-        <div className="input-group"> 
+export const FormLoginComponent = () => {
+  return (
+    <form>
+      <div className="input-group">
+        <label htmlFor="email">E-mail</label>
+        <input type="email" id="email" placeholder="Digite seu email" />
+      </div>
+
+      <div className="input-group">
         <label htmlFor="senha">Senha</label>
-        <input type="password" id="password" placeholder="Digite sua senha"/>
-        
-        </div>
-        <button type="submit">Logar</button>
-        
-    </form>)
-}
+        <input type="password" id="password" placeholder="Digite sua senha" />
+      </div>
+      <button type="submit">Logar</button>
+      <ButtonNewUserComponent />
+    </form>
+  );
+};

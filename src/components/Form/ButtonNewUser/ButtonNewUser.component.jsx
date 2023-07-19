564ee23a37654266cac7a5b4ff53ac0b7d72c4dd
Jefferson Pedro dos Santos Silva <jeffersonpedro300@gmail.com>
2023-07-18 22:25:09 -0300

    Salvando novos usuários no Json server

--- src/components/Form/ButtonNewUser/ButtonNewUser.component.jsx
+++ src/components/Form/ButtonNewUser/ButtonNewUser.component.jsx
@@ -6,28 +6,39 @@ import Modal from "react-bootstrap/Modal";
 
 function ButtonNewUserComponent() {
   const [show, setShow] = useState(false);
+  const [id, setID] = useState(1);
 
   const emailInputElement = useRef();
   const password1InputElement = useRef();
   const password2InputElement = useRef();
-  
+
   const handleClose = () => setShow(false);
 
   const handleShow = () => {
     
-    const data = {
-      email: emailInputElement.current?.value || "",
-      password1: password1InputElement.current?.value || "",
-      password2: password2InputElement.current?.value || "",
-    };
     
-    const testEmail = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(data.email);
-    if (data.password1 === data.password2 && data.password1.length >= 8 && testEmail) {
+    const email = emailInputElement.current?.value || "";
+    const password = password1InputElement.current?.value || "";
+    const password2 = password2InputElement.current?.value || "";
+    
+    
+    const testEmail = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(email);
+    if (password === password2 && password.length >= 8 && testEmail) {
       
-      handleClose();
+      fetch("http://localhost:8000", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+          body: JSON.stringify({id, email, password})
+        }).then(() => {
+          console.log('Post submitted successfully');
+        
+      })
+        setID(id+1);
+        handleClose();
     }
     else {
-      
       setShow(true)
     }
 

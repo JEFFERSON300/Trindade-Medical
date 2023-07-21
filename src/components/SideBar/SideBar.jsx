2bd2f550823bd59535bafe54557cbdabe4448698
Jefferson Pedro <jeffersonpedro300@gmail.com>
2023-07-21 00:11:34 -0300

    Funcionalidade inicial do side-bar

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,50 @@
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
+import {
+  AiFillSignal,
+  AiOutlineLogout,
+  AiOutlinePlusSquare,
+  AiOutlineUnorderedList,
+} from "react-icons/ai";
+
+function Side() {
+  return (
+    <div style={{ display: "flex", height: "100vh", margin: "10px 0" }}>
+      <Sidebar className="sidebar">
+        <Menu>
+          <MenuItem disabled={true}>
+            <h4>Trindade Medical</h4>
+          </MenuItem>
+
+          <div style={{ margin: "60px 0 0 0" }}>
+            <MenuItem disabled={true}> Geral </MenuItem>
+            <MenuItem icon={<AiFillSignal />}> Inicio </MenuItem>
+            <MenuItem icon={<AiOutlineLogout />}> Sair </MenuItem>
+          </div>
+
+          <div>
+            <MenuItem disabled={true}> Pacientes </MenuItem>
+            <MenuItem icon={<AiOutlinePlusSquare />}> Cadastrar </MenuItem>
+            <MenuItem icon={<AiOutlineUnorderedList />}>
+              Listar Prontuário
+            </MenuItem>
+          </div>
+
+          <div>
+            <MenuItem disabled={true}> Exames </MenuItem>
+            <MenuItem icon={<AiOutlinePlusSquare />}>
+              Cadastrar Consulta
+            </MenuItem>
+            <MenuItem icon={<AiOutlinePlusSquare />}>Cadastrar Exame</MenuItem>
+          </div>
+
+          <div>
+            <MenuItem disabled={true}> Esconder Menu </MenuItem>
+            <MenuItem icon={<AiOutlinePlusSquare />}>Botão</MenuItem>
+          </div>
+        </Menu>
+      </Sidebar>
+    </div>
+  );
+}
+
+export default Side;

a1061415cc4a2e9f4e7df14858ae508eb9580d27
Jefferson Pedro dos Santos Silva <jeffersonpedro300@gmail.com>
2023-07-25 10:47:50 -0300

    Implemenando autenticação do usuário via context

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.css";
 import { GlobalStyle } from "./global.style.jsx";
+import { AuthProvider } from "./contexts/auth/auth.context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
